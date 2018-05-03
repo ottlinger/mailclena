e61b2108f622416e0716189338685b1d09bf5dbe
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-03 23:37:08 +0200

    Issue #3: More tests

--- src/test/java/de/aikiit/mailclena/MailConfigurationClass.java
+++ src/test/java/de/aikiit/mailclena/MailConfigurationClass.java
@@ -7,11 +7,27 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MailConfigurationClass {
 
     private static final String HOST = "myHostIsYourHost";
+    private static final String USER = "username";
+    private static final String CRED = "credentials";
 
     @Test
     public void roundtripHostname() {
         final MailConfiguration mailConfiguration = new MailConfiguration();
         mailConfiguration.setHost(HOST);
         assertThat(mailConfiguration.getHost()).isEqualTo(HOST);
+    }
+
+    @Test
+    public void roundtripUsername() {
+        final MailConfiguration mailConfiguration = new MailConfiguration();
+        mailConfiguration.setUsername(USER);
+        assertThat(mailConfiguration.getUsername()).isEqualTo(USER);
+    }
+
+    @Test
+    public void roundtripPassword() {
+        final MailConfiguration mailConfiguration = new MailConfiguration();
+        mailConfiguration.setPassword(CRED);
+        assertThat(mailConfiguration.getPassword()).isEqualTo(CRED);
     }
 }
