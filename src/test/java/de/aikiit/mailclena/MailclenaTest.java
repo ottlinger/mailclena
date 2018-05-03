e9c342bcce1172d449c249a44ca97cc3e449f9f1
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-03 23:00:23 +0200

    Issue #1: First test cycle.

--- src/test/java/de/aikiit/mailclena/MailclenaTest.java
+++ src/test/java/de/aikiit/mailclena/MailclenaTest.java
@@ -1,4 +1,12 @@
-package java.de.aikiit.mailclena;
+package de.aikiit.mailclena;
+
+import de.aikiit.mailclena.MailClena;
+import org.junit.Test;
 
 public class MailclenaTest {
+
+    @Test
+    public void callMain() {
+        MailClena.main();
+    }
 }
