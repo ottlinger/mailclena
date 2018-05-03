657f636ad801674817782dfb19fe4830fa11f49b
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-03 23:31:14 +0200

    Issue #3: Add pjo for configuration

---
+++ src/main/java/de/aikiit/mailclena/MailConfiguration.java
@@ -0,0 +1,10 @@
+package de.aikiit.mailclena;
+
+import lombok.Data;
+
+@Data
+public class MailConfiguration {
+    private String host;
+    private String username;
+    private String password;
+}
