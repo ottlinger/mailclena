ff482993008787349f4aa990234b3e60c852a075
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-03 23:35:10 +0200

    Issue #3: Add test

--- src/test/java/de/aikiit/mailclena/MailConfigurationClass.java
+++ src/test/java/de/aikiit/mailclena/MailConfigurationClass.java
@@ -1,4 +1,17 @@
 package de.aikiit.mailclena;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MailConfigurationClass {
+
+    private static final String HOST = "myHostIsYourHost";
+
+    @Test
+    public void roundtripHostname() {
+        final MailConfiguration mailConfiguration = new MailConfiguration();
+        mailConfiguration.setHost(HOST);
+        assertThat(mailConfiguration.getHost()).isEqualTo(HOST);
+    }
 }
