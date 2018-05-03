2c61d6b71e8d930af3559d363af4fce186a60bcf
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-04 00:08:22 +0200

    Issue #1: Fix test failure

--- src/test/java/de/aikiit/mailclena/MailclenaTest.java
+++ src/test/java/de/aikiit/mailclena/MailclenaTest.java
@@ -2,14 +2,10 @@ package de.aikiit.mailclena;
 
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class MailclenaTest {
 
-    @Test
-    public void callMainWithoutParameters() {
+    @Test(expected = IllegalArgumentException.class)
+    public void callMainWithoutParametersYieldsException() {
         MailClena.main();
-        // to get rid of the warning
-        assertThat(true).isTrue();
     }
 }
