a63a548411ae36096153cd3de2e301b2ba91c628
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-03 23:13:21 +0200

    Issue #1: Hide utility class constructor

--- src/main/java/de/aikiit/mailclena/MailClena.java
+++ src/main/java/de/aikiit/mailclena/MailClena.java
@@ -1,10 +1,11 @@
 package de.aikiit.mailclena;
 
+import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.java.Log;
 
 @Log
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MailClena {
 
     public static void main(String... args) {
