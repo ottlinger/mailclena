208872e51db1179091baa61be1c0cd1bc5173707
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-07 22:09:12 +0200

    Issue #2: Make attributes NonNull

--- src/main/java/de/aikiit/mailclena/MailConfiguration.java
+++ src/main/java/de/aikiit/mailclena/MailConfiguration.java
@@ -1,10 +1,16 @@
 package de.aikiit.mailclena;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
+@Builder
 class MailConfiguration {
+    @NonNull
     private String host;
+    @NonNull
     private String username;
+    @NonNull
     private String password;
 }
