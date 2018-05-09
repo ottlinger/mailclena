781b5e1b109821393c2d87a2db26f54183a73388
Hugo Hirsch <github@hugo-hirsch.de>
2018-05-10 00:37:02 +0200

    Issue #2: Mail interaction with tests

--- src/test/java/de/aikiit/mailclena/mail/MailClientTest.java
+++ src/test/java/de/aikiit/mailclena/mail/MailClientTest.java
@@ -1,19 +1,18 @@
 /**
- MailClena - Copyright (C) 2018, Aiki IT
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
+ * MailClena - Copyright (C) 2018, Aiki IT
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package de.aikiit.mailclena.mail;
 
@@ -24,6 +23,6 @@ public class MailClientTest {
 
     @Test
     public void initWithConfig() {
-        new MailClient(MailConfiguration.builder().build());
+        new MailClient(MailConfiguration.builder().host("h").username("u").password("p").build());
     }
 }
