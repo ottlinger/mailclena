2881b642a76a49962a07f484f6014fff8bf96924
Hugo Hirsch <github@hugo-hirsch.de>
2019-05-02 12:22:11 +0200

    Issue #6: fix unneeded un/boxing

--- src/main/java/de/aikiit/mailclena/mail/MailClient.java
+++ src/main/java/de/aikiit/mailclena/mail/MailClient.java
@@ -126,7 +126,7 @@ public class MailClient {
                 AtomicLong mailSize = new AtomicLong(0L);
                 messages.forEach(message -> {
                     try {
-                        Long messageSize = Long.valueOf(message.getSize());
+                        long messageSize = message.getSize();
                         log.debug("Marking for deletion " + messageSize + " bytes, " + message.getSubject() + " From: " + Arrays.toString(message.getFrom()));
                         message.setFlag(Flags.Flag.DELETED, true);
                         mailSize.addAndGet(messageSize);
