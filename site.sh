88485b12e3dbad7c151cc95d924f836fed7a7f41
Hugo Hirsch <github@hugo-hirsch.de>
2022-03-02 22:28:21 +0100

    Change site gen

--- site.sh
+++ site.sh
@@ -3,6 +3,6 @@ echo "Generating new mvn site ..."
 mvn clean site:site 
 echo "DONE - ready to commit and push"
 cp -rf target/site/* docs
-git add docs/*
+git add docs/
 git commit -a -m "Issue #7: Update site"
 git push
