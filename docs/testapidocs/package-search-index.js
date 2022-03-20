56a913b4a096ce7fa98cca8a5a028900bd18b901
Hugo Hirsch <github@hugo-hirsch.de>
2022-03-20 17:12:27 +0100

    Issue #7: Update site

--- docs/testapidocs/package-search-index.js
+++ docs/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.aikiit.mailclena"},{"l":"de.aikiit.mailclena.mail"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"de.aikiit.mailclena"},{"l":"de.aikiit.mailclena.mail"}]
