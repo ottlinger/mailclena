1735031942427be4deb330a1236a9d17ab2bbd6a
Hugo Hirsch <github@hugo-hirsch.de>
2024-02-20 16:58:02 +0100

    Clean logs

--- site.sh
+++ site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Generating new mvn site ..."
-./mvnw clean org.pitest:pitest-maven:mutationCoverage site:site 
+./mvnw -B clean org.pitest:pitest-maven:mutationCoverage site:site 
 echo "DONE - ready to commit and push"
 cp -rf target/site/* docs
 git add -f docs/
