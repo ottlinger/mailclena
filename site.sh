9b3a2fd5b201be74d12e93610f7793ef50d9620a
Hugo Hirsch <github@hugo-hirsch.de>
2025-09-25 11:30:16 +0200

    Add note about failing builds

--- site.sh
+++ site.sh
@@ -5,4 +5,5 @@ echo "DONE - ready to commit and push"
 cp -rf target/site/* docs
 git add -f docs/
 git commit -a -m "Issue #7: Update site"
+# DEVHINT: This may fail if any changes/commits were run in the background on the repo, e.g. dependabot/GHA automation scripts
 git push
